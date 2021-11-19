1700944eedf605eb4698537c03241deb8eb97670
iu.monogosheva <iu.monogosheva@tensor.ru>
2021-11-19 21:23:01 +0700

    Создала проект

---
+++ FitAngel/FitAngel/FitAngelApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitAngelApp.swift
+//  FitAngel
+//
+//  Created by Ирина Моногошева on 19.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct FitAngelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
